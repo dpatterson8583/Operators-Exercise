30de07c2f909ff8bb5964ddfc366dd53f2ce1398
dpatterson8583 <dpatterson8583@gmail.com>
2023-04-16 23:22:51 -0500

    Initial commit for Operators-Exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Exercise 1
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+            if (a == 17 && b== 4)
+            {
+                Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+            }
+
+            double r;
+            Console.WriteLine("What is the radius of your circle? > ");
+            r = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"The area of a circle with a radius of {r} is {AreaOfCircle(r)} ");
+        }
+
+        public static double AreaOfCircle(double radius)
+        { 
+            return radius * radius * Math.PI;
+
         }
     }
 }
