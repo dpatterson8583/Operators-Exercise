94c6fe8c510c4b6bfdf6edc53bedac7f20935c54
dpatterson8583 <dpatterson8583@gmail.com>
2023-05-20 10:48:48 -0500

    Resubmission post remediation. 5/20

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -9,13 +9,21 @@
             int b = 4;
             int quotient = a / b;
             int remainder = a % b;
+
+            Console.WriteLine($"The sum of  {a} + {b} is {a + b}");
+            Console.WriteLine($"The difference of  {a} - {b} is {a - b}");
+            Console.WriteLine($"The product of  {a} * {b} is {a * b}");
+
             if (a == 17 && b== 4)
             {
                 Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
             }
 
+            Console.WriteLine("----------------------------------------------------------------------------------");
+
+            //Exercise 2
             double r;
-            Console.WriteLine("What is the radius of your circle? > ");
+            Console.Write("What is the radius of your circle? > ");
             r = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine($"The area of a circle with a radius of {r} is {AreaOfCircle(r)} ");
